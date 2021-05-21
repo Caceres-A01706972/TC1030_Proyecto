e9000c896e9f45625003e5cefabdb893d082e445
Ricardo Caceres <rcaceres1125@gmail.com>
2021-05-20 20:59:24 -0600

    Carga archivo con basetimes

---
+++ basetimes.h
@@ -0,0 +1,57 @@
+/* Este archivo unicamente contien los tiempos base para cada prueba, estilo y genero.
+Obtenidos de: https://resources.fina.org/fina/document/2021/01/11/43122943-963d-47da-a7d1-b0ad34e76ec9/fina_points_base_times_-_scm_and_lcm_2021.pdf
+*/
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+//Para Piscina Corta (25m)
+float short_cincuentaLibre[2] = {20.24,22.93};
+float short_cienLibre[2] = {44.94,50.25};
+float short_dosLibre[2] = {99.37,110.43};
+float short_cuatroLibre[2] = {212.25,233.92};
+float short_ocho[2] = {443.42,479.34};
+float short_milqui[2] = {848.06,918.01};
+
+float short_cincuentaDorso[2] = {22.22,25.67};
+float short_cienDorso[2] = {48.88,54.89};
+float short_dosDorso[2] = {105.63,119.23};
+
+float short_cincuentaPecho[2] = {25.25,28.56};
+float short_cienPecho[2] = {55.61,62.36};
+float short_dosPecho[2] = {120.16,134.57};
+
+float short_cincuentaFly[2] = {21.75,24.38};
+float short_cienFly[2] = {48.08,54.61};
+float short_dosFly[2] = {108.24,119.61};
+
+float short_cienCombi[2] = {50.26,56.51};
+float short_dosCombi[2] = {109.63,121.86};
+float short_cuatroCombi[2] = {234.81,258.94};
+
+//Para Piscina Larga (50m)
+float long_cincuentaLibre[2] = {20.91,23.67};
+float long_cienLibre[2] = {46.91,51.71};
+float long_dosLibre[2] = {102.00,112.98};
+float long_cuatroLibre[2] = {220.07,236.46};
+float long_ocho[2] = {452.12,484.79};
+float long_milqui[2] = {871.02,920.48};
+
+float long_cincuentaDorso[2] = {24.00,26.98};
+float long_cienDorso[2] = {51.85,57.57};
+float long_dosDorso[2] = {111.92,123.35};
+
+float long_cincuentaPecho[2] = {25.95,29.40};
+float long_cienPecho[2] = {56.88,64.13};
+float long_dosPecho[2] = {126.12,139.11};
+
+float long_cincuentaFly[2] = {22.27,24.43};
+float long_cienFly[2] = {49.50,55.48};
+float long_dosFly[2] = {110.73,121.81};
+
+float long_dosCombi[2] = {114.00,126.12};
+float long_cuatroCombi[2] = {243.84,266.36};
